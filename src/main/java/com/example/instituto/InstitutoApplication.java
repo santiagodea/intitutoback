0046efea17081e3eecc02cbc4e11f7db87c6d9c7
Santiago De Andrea <sntgdeandrea@gmail.com>
2020-02-17 19:40:32 -0300

    primer commit

---
+++ src/main/java/com/example/instituto/InstitutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.instituto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/instituto/InstitutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instituto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
