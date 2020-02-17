3dd89120aeb1e9dd30530e4dbc29fcc7ea0e064c
Santiago De Andrea <sntgdeandrea@gmail.com>
2020-02-17 19:53:58 -0300

    ...

--- src/main/java/com/example/instituto/InstitutoApplication.java
+++ src/main/java/com/example/instituto/InstitutoApplication.java
@@ -2,18 +2,20 @@ package com.example.instituto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class InstitutoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InstitutoApplication.class, args);
-	}
-
-	@RequestMapping("/")
+    @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hola, esta es la API del Instituto";
+      return "Hello World!";
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(InstitutoApplication.class, args);
+    }
 }
