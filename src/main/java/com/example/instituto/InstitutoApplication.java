9a31a7c5aa98d573c68f1a5aba831bd9843559eb
Santiago De Andrea <sntgdeandrea@gmail.com>
2020-02-17 20:12:15 -0300

    mod pom

--- src/main/java/com/example/instituto/InstitutoApplication.java
+++ src/main/java/com/example/instituto/InstitutoApplication.java
@@ -2,20 +2,23 @@ package com.example.instituto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
 @SpringBootApplication
 public class InstitutoApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(InstitutoApplication.class, args);
-    }
+	}
+}
+
+@RestController
+class HelloController{
+
+	@GetMapping(value="/")
+	String hello(){
+		return "Hola esta es la API del Instituto";
+	}
+
 }
